2e4ee067310b0a038f3506532e03ae0ceddbdbe2
Alex Dorogensky <amazinglifetime@gmail.com>
2019-04-01 08:23:48 -0500

    Add spring boot application, UserController with empty getUsers() service operation and test for it

---
+++ src/main/java/com/github/adorogensky/workshop/alpha/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.github.adorogensky.workshop.alpha.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+	@GetMapping("/users")
+	public void getUsers() {
+
+	}
+}
