da8b38b01313a7857d33b3e0d596e605802db001
Alex Dorogensky <amazinglifetime@gmail.com>
2019-04-07 17:58:20 -0500

    Redirect / to /swagger-ui.html

--- src/test/java/com/github/adorogensky/workshop/alpha/controller/SpringfoxSwaggerUITest.java
+++ src/test/java/com/github/adorogensky/workshop/alpha/controller/SpringfoxSwaggerUITest.java
@@ -1,6 +1,8 @@
 package com.github.adorogensky.workshop.alpha.controller;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.springframework.test.util.ReflectionTestUtils.getField;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -21,6 +24,15 @@ public class SpringfoxSwaggerUITest {
 
 	@Autowired
 	private MockMvc mvc;
+
+	@Test
+	public void rootPathDisplaysSwaggerUIPage() throws Exception {
+		MvcResult result = mvc.perform(
+			get("/").contentType(MediaType.APPLICATION_JSON)
+		).andExpect(status().is3xxRedirection()).andReturn();
+
+		assertEquals("/swagger-ui.html", getField(result.getModelAndView().getView(), "url"));
+	}
 
 	@Test
 	public void swaggerUIPageDisplays() throws Exception {
