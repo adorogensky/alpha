81da08919324259cc1d8940194ab48f0deadba7a
Alex Dorogensky <amazinglifetime@gmail.com>
2019-04-07 17:25:49 -0500

    Add test to verify that swagger ui page displays

---
+++ src/test/java/com/github/adorogensky/workshop/alpha/controller/SpringfoxSwaggerUITest.java
@@ -0,0 +1,34 @@
+package com.github.adorogensky.workshop.alpha.controller;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class SpringfoxSwaggerUITest {
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Test
+	public void swaggerUIPageDisplays() throws Exception {
+		MvcResult result = mvc.perform(
+			get("/swagger-ui.html").contentType(MediaType.APPLICATION_JSON)
+		).andExpect(status().isOk()).andReturn();
+
+		String htmlResponse = result.getResponse().getContentAsString();
+		assertTrue(htmlResponse.contains("<div id=\"swagger-ui\"></div>"));
+	}
+}
