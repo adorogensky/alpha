c2d0aee88f3fbb0004d50d9a0880ab1d3ce99d83
Alex Dorogensky <amazinglifetime@gmail.com>
2019-05-09 17:34:27 -0500

    Add CommitTransactionBeforeTest2

---
+++ src/test/java/com/github/adorogensky/workshop/alpha/controller/CommitTransactionBeforeTest2.java
@@ -0,0 +1,56 @@
+package com.github.adorogensky.workshop.alpha.controller;
+
+import com.github.adorogensky.workshop.alpha.domain.entity.User;
+import com.github.adorogensky.workshop.alpha.repository.UserRepository;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import java.math.BigInteger;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CommitTransactionBeforeTest2 {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	private Integer userId;
+
+	private Integer userCount;
+
+	@Before
+	public void setUp() {
+		userCount = userRepository.findAll().size();
+		User addUser = new User();
+		addUser.setLogin("test2");
+		addUser.setPassword("test2");
+		userRepository.save(addUser);
+		userId = addUser.getId();
+	}
+
+	@Test
+	public void findNewlyAddedUser() {
+		List userCount = entityManager.createNativeQuery("SELECT COUNT(id) FROM alpha.user_profile").getResultList();
+		assertEquals(1, userCount.size());
+		assertEquals(BigInteger.valueOf(this.userCount + 1), userCount.get(0));
+	}
+
+	@After
+	public void tearDown() {
+		userRepository.deleteById(userId);
+	}
+}
